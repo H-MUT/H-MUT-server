92c3fb628400ae2bef78113f5bb096e3f519f107
김민준 <sanbonai06@naver.com>
2023-03-15 14:08:15 +0900

    :rocket: 멀티 모듈 초기 세팅

---
+++ Api/src/main/java/hongik/hmut/HmutApiApplication.java
@@ -0,0 +1,5 @@
+package hongik.hmut;
+
+public class HmutApiApplication {
+
+}
---
+++ src/test/java/hongik/hmut/hmut/HmutApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.hmut.hmut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
