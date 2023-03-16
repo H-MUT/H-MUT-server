0ed967d30be21ac179cc6b18284379955c89b9fc
김민준 <sanbonai06@naver.com>
2023-03-16 17:53:19 +0900

    :rocket: feat: 멀티모듈 도메인 설정

---
+++ Domain/src/main/java/hongik/hmut/domain/domains/user/User.java
@@ -0,0 +1,27 @@
+package hongik.hmut.domain.domains.user;
+
+
+import hongik.hmut.domain.domains.AbstractTimeStamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "tbl_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends AbstractTimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String phoneNum;
+}
